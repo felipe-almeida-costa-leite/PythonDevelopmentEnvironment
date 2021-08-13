f18892fc6c40ccc5fc89f2bcdbc964137ab7d85d
felipe.leite <felipecostasilvaleite@hotmail.com>
2021-08-13 15:32:02 -0300

    Commit

---
+++ src/main/soma.py
@@ -0,0 +1,26 @@
+# !/usr/bin/python3
+# encoding: ISO-8859-1
+"""
+Função criada para testar o schema de commit
+"""
+
+
+def soma(num_1: int, num_2: int) -> int:
+    """
+    Retorna a soma de dois números.
+        Parameters:
+            :param num_1: Primeiro Numero da Soma
+            :type num_1: int
+            :param num_2: Segundo Numero da Soma
+            :type num_2: int
+        Returns:
+            :return soma_dois_numeros: Soma dos dois números
+            :rtype soma_dois_numeros: int
+        Exemples:
+            >>> num_3 = 1
+            >>> num_4 = 2
+            >>> soma(num_1, num_2)
+            3
+    """
+    soma_dois_numeros: int = num_1 + num_2
+    return soma_dois_numeros
